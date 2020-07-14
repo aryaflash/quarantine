83fdccb348735bc44ea1c197b5990e2a618f84c1
aryaflash <aryaflash001@gmail.com>
2020-07-14 20:12:16 +0530

    adding new staff modules

--- arya/serializers.py
+++ arya/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
-from .models import Manager
+from .models import Manager, Staff
 
 class ManagerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Manager
         fields = ['name', 'gmail', 'phoneNumber', 'address', 'password']
 
-
+class StaffSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Staff
+        fields = ['name', 'gmail', 'phoneNumber', 'address', 'manager']
--- arya/urls.py
+++ arya/urls.py
@@ -3,8 +3,11 @@ from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('arya/managers/', views.ManagerList.as_view(), name = 'Managers List'),
-    path('arya/managers/<int:pk>/', views.ManagerDetail.as_view(), name = 'Manager Detail'),
+    path('arya/managers/', views.ManagerList.as_view(), name = 'Managers_List'),
+    path('arya/managers/<int:pk>/', views.ManagerDetail.as_view(), name = 'Manager_Detail'),
+    path('arya/staff/', views.StaffList.as_view(), name = 'Staff_List'),
+    path('arya/staff/<int:pk>/', views.StaffDetail.as_view(), name = 'Staff_Detail'),
+    path('arya/<int:pk>/staffundermanager/', views.StaffUnderManager.as_view(), name = 'StaffUnderManage')
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
